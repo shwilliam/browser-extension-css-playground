df5750660a78adaadaea59c6c34faea64db8e4fb
William L <w-lindvall@outlook.com>
2020-04-04 11:42:05 -0700

    init editor

---
+++ popup/editor.js
@@ -0,0 +1,30 @@
+let cachedStylesheet
+const inputEl = document.getElementById('__css_playground_editor_input__')
+const toggleEl = document.getElementById('__css_playground_editor_toggle__')
+const toggleLabelOn = 'Hide custom styles'
+const toggleLabelOff = 'Preview custom styles'
+
+inputEl.addEventListener('keydown', e => {
+  const styles = e.target.value
+
+  if (toggleEl.innerText === toggleLabelOn) {
+    browser.tabs.removeCSS({code: cachedStylesheet})
+    browser.tabs.insertCSS({code: styles})
+    cachedStylesheet = styles
+  }
+})
+
+toggleEl.addEventListener('click', () => {
+  if (cachedStylesheet) {
+    // hide custom styles
+    browser.tabs.removeCSS({code: cachedStylesheet})
+    cachedStylesheet = null
+    toggleEl.innerText = toggleLabelOff
+  } else {
+    // show custom styles
+    const styles = inputEl.value
+    cachedStylesheet = styles
+    browser.tabs.insertCSS({code: styles})
+    toggleEl.innerText = toggleLabelOn
+  }
+})
